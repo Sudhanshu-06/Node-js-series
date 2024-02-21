6afd456c6545547c4d2db70769ebb6575eed56e4
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-02-21 22:23:49 +0530

    push

--- server1.js
+++ server1.js
@@ -21,27 +21,76 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const Person = require('./models/Person');
+const MenuItem = require('./models/MenuItem');
 
 app.get('/', function (req, res) {
   res.send('Welcome to Sudhanshu world ...how can i help you')
 });
 
-app.post('/person',(req,res) => {
+//POST route to add a person
+app.post('/person',async(req,res) => {
 
-  const data = req.body
+  try{
+    const data = req.body
+    //Create a new Person document using the Mongoose model
+    const newPerson = new Person(data);
+    //Save the new person to the database
+    const response = await newPerson.save();
+    console.log('data saved');
+    res.status(200).json(response);
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({error:'Internal Server Error'})
+  }
 
-  const newPerson = new Person(data);
-
-  newPerson.save((error,savedperson) =>{
-    if(error){
-      console.log('Error saving person:',error);
-      res.status(500).json({error: 'Internal server error'})
-    }else{
-      console.log('data saved successfully');
-      res.status(200).json(savedpersonperson);
-    }
-  })
+  
 })
+
+// GET method to get the person
+app.get('/person',async(req,res) =>{
+  try{
+    const response = await Person.find();
+    console.log('data fetched');
+    res.status(200).json(response);
+
+  }catch(err){
+    console.log('data saved');
+    res.status(200).json({error:'Internal server Error'});
+
+  }
+})
+
+//Post Method to add a Menu Item
+app.post('/menu',async(req,res)=>{
+  try{
+    const data = req.body
+    const newMenu = new MenuItem(data);
+    const response = await newMenu.save();
+    console.log('data saved');
+    res.status(200).json(response);
+
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({error:'Internal Server Error'});
+  }
+})
+
+//GET method to get the Menu Items
+app.get('/menu',async(req,res) =>{
+  try{
+    const response = await MenuItem.find();
+    console.log('data fetched');
+    res.status(200).json(data);
+
+  }catch(err){
+    console.log('data saved');
+    res.status(200).json({error:'Internal server Error'});
+
+  }
+})
+
 
 app.listen(3000,()=>{
     console.log('listening port is 3000')
