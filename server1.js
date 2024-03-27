97cd8d0ab94666672256d76f7113feb472074a9c
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-03-27 11:14:31 +0530

    push

--- server1.js
+++ server1.js
@@ -20,75 +20,105 @@ const db = require('./db');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const Person = require('./models/Person');
-const MenuItem = require('./models/MenuItem');
+
+
 
 app.get('/', function (req, res) {
   res.send('Welcome to Sudhanshu world ...how can i help you')
 });
 
 //POST route to add a person
-app.post('/person',async(req,res) => {
 
-  try{
-    const data = req.body
-    // Create a new Person document using the Mongoose model
-    const newPerson = new Person(data);
-    // Save the new person to the database
-    const response = await newPerson.save();
-    console.log('data saved');
-    res.status(200).json(response);
-  }
-  catch(err){
-    console.log(err);
-    res.status(500).json({error:'Internal Server Error'})
-  }
+// app.post('/person',async(req,res) => {
+
+//   try{
+//     const data = req.body
+//     // Create a new Person document using the Mongoose model
+//     const newPerson = new Person(data);
+//     // Save the new person to the database
+//     const response = await newPerson.save();
+//     console.log('data saved');
+//     res.status(200).json(response);
+//   }
+//   catch(err){
+//     console.log(err);
+//     res.status(500).json({error:'Internal Server Error'})
+//   }
 
   
-})
+// })
 
 // GET method to get the person
-app.get('/person',async(req,res) =>{
-  try{
-    const data = await Person.find();
-    console.log('data fetched');
-    res.status(200).json(data);
 
-  }catch(err){
-    console.log(err);
-    res.status(200).json({error:'Internal server Error'});
+// app.get('/person',async(req,res) =>{
+//   try{
+//     const data = await Person.find();
+//     console.log('data fetched');
+//     res.status(200).json(data);
 
-  }
-});
+//   }catch(err){
+//     console.log(err);
+//     res.status(200).json({error:'Internal server Error'});
 
-app.post('/MenuItem',async(req,res) => {
+//   }
+// });
 
-  try{
-    const data = req.body
-    const newMenu = new MenuItem(data);
-    const response = await newMenu.save();
-    console.log('data saved');
-    res.status(200).json(data);
-  }
-  catch(err){
-    console.log(err);
-    res.status(500).json({error:'Internal Server Error'})
-  }
+// app.post('/MenuItem',async(req,res) => {
+
+//   try{
+//     const data = req.body
+//     const newMenu = new MenuItem(data);
+//     const response = await newMenu.save();
+//     console.log('data saved');
+//     res.status(200).json(data);
+//   }
+//   catch(err){
+//     console.log(err);
+//     res.status(500).json({error:'Internal Server Error'})
+//   }
 
   
-})
-app.get('/MenuItem',async(req,res) =>{
-  try{
-    const data = await MenuItem.find();
-    console.log('data fetched');
-    res.status(200).json(data);
+// })
 
-  }catch(err){
-    console.log('data saved');
-    res.status(200).json({error:'Internal server Error'});
+// app.get('/MenuItem',async(req,res) =>{
+//   try{
+//     const data = await MenuItem.find();
+//     console.log('data fetched');
+//     res.status(200).json(data );
 
-  }
-})
+//   }catch(err){
+//     console.log('data saved');
+//     res.status(200).json({error:'Internal server Error'});
+
+//   }
+// })
+
+// app.get('/person/:workType',async(req,res)=>{
+//   try{
+//     const workType = req.params.workType;
+//     if(workType == 'chef' || workType =='waiter'|| workType =='manager' ){
+//       const response = await Person.find({work:workType});
+//       console.log('response fetched');
+//       res.status(200).json(response);
+//     }else{
+//       res.status(404).json({error: 'Invalid work type'});
+//     }
+
+//   }catch(err){
+//     console.log('data saved');
+//     res.status(500).json({error:'Internal server Error'});
+
+//   }
+// })
+
+
+// Import the router files
+const personRoutes= require('./routes/personRoutes');
+const MenuItemRoutes = require('./routes/menuItemRoutes');
+
+// Use the routers
+app.use('/person',personRoutes);
+app.use('./menuItem',MenuItemRoutes)
 
 app.listen(3000,()=>{
     console.log('listening port is 3000')
